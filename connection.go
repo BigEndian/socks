38ec5de80b0cf7a8cf282995bfbb8dda51f24f54
Eric Carr <superjapanfreak@gmail.com>
2013-01-06 17:54:04 -0800

    cleaned up a bit, added read timeouts

--- connection.go
+++ connection.go
@@ -6,8 +6,32 @@ import (
    "os"
    "net"
    "log"
+   "time"
 )
 
+func printBytesFormatted(buf []byte, length, line_length uint, hex bool) {
+   as_decimal := ""
+   as_hex := ""
+   var idx uint = 0
+   print("\t")
+   for idx = 0; idx < length; idx++ {
+      if (idx + 1) % 30 == 0 {
+         as_hex += "\n\t"
+         as_decimal += "\n\t"
+      }
+      as_hex += fmt.Sprintf("%3X", buf[idx])
+      as_decimal += fmt.Sprintf("%3d", buf[idx])
+   }
+   
+   if hex {
+      print(as_hex)
+   } else {
+      print(as_decimal)
+   }
+   if (idx + 1) % 30 != 0 {
+      println()
+   }
+}
 type Connection struct {
    tcp_conn *net.TCPConn
    auth_header *Header
@@ -28,7 +52,7 @@ func (conn *Connection) Handle(count *ConnectionCount) error {
    stdout := conn.stdout
    //stderr := conn.stderr
    debug  := conn.debug
-   stdout.Printf("Connection.Handle (%+v) received connection %+v\n", conn, conn.tcp_conn)
+   stdout.Printf("Connection.Handle received connection %+v\n", conn, conn.tcp_conn)
    conn.tcp_conn.SetReadBuffer(1024)
    conn.tcp_conn.SetWriteBuffer(1024)
    read_buffer := make([]byte, 1024)
@@ -43,24 +67,25 @@ func (conn *Connection) Handle(count *ConnectionCount) error {
       for i := 0; i < cap(read_buffer); i+=1 {
          read_buffer[i] = 0
       }
+
+      // Set a 5 second timeout for the read
+      now := time.Now()
+      conn.tcp_conn.SetReadDeadline(now.Add(5 * time.Second))
       count, err := conn.tcp_conn.Read(read_buffer)
-      if err != io.EOF && err != nil {
-         panic(err)
-      } else if err == io.EOF {
-         debug.Printf("Connection.Handle (%+v) caught an EOF on TCPConn %+v\n", conn, conn.tcp_conn)
-         break
-      }
-      debug.Printf("Iterating buffer. . .\n\t")
-      var buffer_index int
-      for buffer_index = 0; buffer_index < count; buffer_index+=1 {
-         fmt.Printf("%d ", read_buffer[buffer_index])
-         if (buffer_index + 1) % 30 == 0 {
-            fmt.Printf("\n\t")
+      if err != nil {
+         if err == io.EOF {
+            debug.Printf("Connection.Handle caught an EOF on TCPConn %+v\n", conn, conn.tcp_conn)
+            break
+         } else if err.(net.Error).Timeout() {
+            debug.Printf("Connection.Handle timed out, connection closed")
+            return nil
+         } else {
+            panic(err)
          }
       }
-      if (buffer_index + 1) % 30 != 0 {
-         println()
-      }
+
+      debug.Println("Iterating buffer. . .")
+      printBytesFormatted(read_buffer, (uint)(count), 30, false) // 30 lines dec
 
       var socks_header *Header
       var req_header *RequestHeader
